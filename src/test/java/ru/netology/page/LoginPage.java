4cdbb4cea85b0a1dc0ee0cbdacc0b983dd42469b
Siumbel <sumbelragipova@gmail.com>
2022-11-16 21:26:18 +0300

    Inition commit

---
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -0,0 +1,33 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+/**
+ * @author Siumbel
+ */
+public class LoginPage {
+
+  private SelenideElement LOGIN = $x("//input[@name='login']");
+  private SelenideElement PASSWORD = $x("//input[@name='password']");
+  private SelenideElement BUTTON = $x("//button[@data-test-id='action-login']");
+
+  public VeryficationPage sucessfulLogin(DataHelper.AuthInfo info) {
+    LOGIN.setValue(info.getLogin());
+    PASSWORD.setValue(info.getPassword());
+    BUTTON.click();
+    return new VeryficationPage();
+  }
+/*
+  public void wrongLogin() {
+    LOGIN.setValue();
+    PASSWORD.setValue();
+    BUTTON.click();
+  }
+
+ */
+
+}
